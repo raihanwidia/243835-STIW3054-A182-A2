397a22229263cacfac1b1e7e3654a58173351f64
Raihan Widia 243835 <raihankirana@gmail.com>
2019-04-01 16:06:02 +0800

    First Commit by 243835

---
+++ src/main/java/com/STIW3054/assigment2/App.java
@@ -0,0 +1,13 @@
+package com.STIW3054.assigment2;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
